6e019697cb189a425c75d3592593c27950017134
25Dec <thiennhannguyenhuynh@gmail.com>
2022-11-16 14:40:09 +0700

    exercise 1-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import "typeface-roboto";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import AndroidIcon from "@mui/icons-material/Android";
 
-function App() {
-  const [count, setCount] = useState(0)
+const buttonStyle = { margin: 10 };
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function toggleColor(setter, value) {
+	setter(value === "default" ? "primary" : "default");
 }
 
-export default App
+export default function App() {
+	const [contained, setContained] = useState("defaults");
+	const [text, setText] = useState("default");
+	const [outlined, setOutlined] = useState("default");
+	const [icon, setIcon] = useState("default");
+
+	return (
+		<Grid container>
+			<Grid
+				item
+				component={Button}
+				variant="contained"
+				style={buttonStyle}
+				color={contained}
+				onClick={() => toggleColor(setContained, contained)}
+			>
+				Contained
+			</Grid>
+			<Grid
+				item
+				component={Button}
+				variant="contained"
+				style={buttonStyle}
+				color={text}
+				onClick={() => toggleColor(setText, text)}
+			>
+				Text
+			</Grid>
+			<Grid
+				item
+				component={Button}
+				variant="outlined"
+				style={buttonStyle}
+				color={outlined}
+				onClick={() => toggleColor(setOutlined, outlined)}
+			>
+				Text
+			</Grid>
+			<Grid item component={IconButton} style={buttonStyle} color={icon} onClick={() => toggleColor(setIcon, icon)}>
+				<AndroidIcon />
+			</Grid>
+		</Grid>
+	);
+}
