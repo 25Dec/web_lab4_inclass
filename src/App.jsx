a92767c175c53324dc49c51ed43fde19f5415247
25Dec <thiennhannguyenhuynh@gmail.com>
2022-11-16 08:40:02 +0700

    exercise 1-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function fetchUser() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve({ id: 1, name: "Adam" });
+		}, 1000);
+	});
 }
 
-export default App
+function App() {
+	const [id, setId] = useState("loading...");
+	const [name, setName] = useState("loading...");
+
+	useEffect(() => {
+		fetchUser().then((user) => {
+			setId(user.id);
+			setName(user.name);
+		});
+	});
+
+	return (
+		<>
+			<p>ID: {id}</p>
+			<p>Name: {name}</p>
+		</>
+	);
+}
+
+export default App;
